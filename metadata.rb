4109b9378a4909affd593cdfa24aee0c7171a80b
Andrea Campi <andrea.campi@zephirworks.com>
2012-04-17 23:40:24 +0200

    Add dependencies.

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,7 @@ license          "Apache 2.0"
 description      "Installs/Configures application_java"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+%w{ application java tomcat }.each do |cb|
+  depends cb
+end
