fe4f2c4d182615d6c285398f04e77c1caf1d2288
Andrea Campi <andrea.campi@zephirworks.com>
2012-04-20 12:55:57 +0200

    Fix the description.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 maintainer       "ZephirWorks"
 maintainer_email "andrea.campi@zephirworks.com"
 license          "Apache 2.0"
-description      "Installs/Configures application_java"
+description      "Deploys and configures Java-based applications"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
