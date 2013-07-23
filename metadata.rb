8405664663503e4b974669f42696de62f1272323
jtimberman <joshua@opscode.com>
2013-07-23 16:27:56 -0700

    increment version for development

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "andrea.campi@zephirworks.com"
 license          "Apache 2.0"
 description      "Deploys and configures Java-based applications"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.0.0"
+version          "2.0.1"
 
 %w{ java tomcat }.each do |cb|
   depends cb
